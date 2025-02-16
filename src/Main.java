0e20cec93f0b296ca66ce2d7be3b82f509b7a505
Marcos Lima <marcoslkz@gmail.com>
2025-02-16 20:04:57 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa("João", 30);
-        Pessoa aluno1 = new Aluno("Carlos", 22, "Programação");
-        Pessoa professor1 = new Professor("Gabriel", 40, "CIEP 476");
+        //Pessoa aluno1 = new Aluno("Carlos", 22, "Programação");
+        //Pessoa professor1 = new Professor("Gabriel", 40, "CIEP 476");
         
         pessoa1.apresentar();
-        aluno1.apresentar();
-        professor1.apresentar();
+        //aluno1.apresentar();
+        //professor1.apresentar();
     }
 }
